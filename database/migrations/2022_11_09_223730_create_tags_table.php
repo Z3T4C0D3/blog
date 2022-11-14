002f9aaec2c3407c70942d21967ad0992852c04d
Z3T4C0D3 <victorus_11@hotmail.com>
2022-11-13 23:51:44 -0600

    Show-Libros

--- database/migrations/2022_11_09_223730_create_tags_table.php
+++ database/migrations/2022_11_09_223730_create_tags_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id('id_tag');
+            $table->id();
+
             $table->string('describeTag');
             $table->string('slugTag');
+            
+        
             $table->timestamps();
         });
     }
