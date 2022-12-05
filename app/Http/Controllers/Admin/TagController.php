37f6abb0867ea42a19a22c2fdadebbb4d5d19988
Z3T4C0D3 <victorus_11@hotmail.com>
2022-12-04 20:37:13 -0600

    CRUD-Etiquetas

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\tags;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = tags::all();
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+       $request -> validate([
+            'describeTag' => "required",
+            'slugTag' => "required|unique:tags",
+        ]);
+        $tag = tags::create($request->all());
+        return redirect()->route('admin.tags.index', $tag)->with('alertCreated', 'La etiqueta se creo con éxito'); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tags $tag)
+    {
+        return view('admin.tags.show', compact('tag'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tags $tag)
+    {
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tags $tag)
+    {
+        $request -> validate([
+            'describeTag' => "required",
+            'slugTag' => "required|unique:tags,slugTag,$tag->id",
+        ]);
+
+        $tag ->update($request->all());
+        return redirect()->route('admin.tags.edit', $tag)->with('alertEdit', 'La etiqueta se actualizo con éxito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tags $tag)
+    {
+        $tag->delete();
+        return redirect()->route('admin.tags.index')->with('alertDelete', 'La etiqueta se eliminó con éxito');
+    }
+}
